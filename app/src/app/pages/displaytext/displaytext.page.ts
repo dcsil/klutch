e597318ea382a5a66bb0d61736b17dbafd0e7f6c
Shawnee Rizqa <shawneerizqa@MacBook-Pro-2.local>
2019-10-22 18:02:54 -0400

    add first three pages of app

---
+++ app/src/app/pages/displaytext/displaytext.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-displaytext',
+  templateUrl: './displaytext.page.html',
+  styleUrls: ['./displaytext.page.scss'],
+})
+export class DisplaytextPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
