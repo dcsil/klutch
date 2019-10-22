a77b9c84e1002b0c3e1f41c0e9698f3fd3d4f6ea
Shawnee Rizqa <shawneerizqa@MacBook-Pro-2.local>
2019-10-22 18:04:51 -0400

    deleted home directory

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
+  { path: 'displaytext', loadChildren: './pages/displaytext/displaytext.module#DisplaytextPageModule' },
+  { path: 'success', loadChildren: './pages/success/success.module#SuccessPageModule' },
 ];
 
 @NgModule({
