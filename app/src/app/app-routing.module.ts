09e7917b3e0c57c4bfd866dbd6b83037fbbf6ad1
Shawnee Rizqa <shawneerizqa@MacBook-Pro-2.local>
2019-10-23 15:41:47 -0400

    initial version of welcome and display text pages

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)},
+  // { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
   { path: 'displaytext', loadChildren: './pages/displaytext/displaytext.module#DisplaytextPageModule' },
-  { path: 'success', loadChildren: './pages/success/success.module#SuccessPageModule' },
+  // { path: 'success', loadChildren: './pages/success/success.module#SuccessPageModule' },
 ];
 
 @NgModule({
--- app/src/app/pages/welcome/welcome.page.ts
+++ app/src/app/pages/welcome/welcome.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ActionSheetController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Camera, PictureSourceType } from '@ionic-native/camera/ngx';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  srcType: string;
+  cameraSourceType: PictureSourceType;
+  selectedImage: string;
+
+  constructor(private camera: Camera, private router: Router, private storage: Storage) { }
 
   ngOnInit() {
   }
+
+  getPicture(srcType) {
+    if (srcType == 'CAMERA')
+    {
+      this.cameraSourceType = this.camera.PictureSourceType.CAMERA;
+    }
+    else if (srcType == 'PHOTOLIBRARY')
+    {
+      this.cameraSourceType = this.camera.PictureSourceType.PHOTOLIBRARY;
+    }
+
+    console.log(this.cameraSourceType);
+    // this.router.navigateByUrl(`/displaytext/45`)
+    
+
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.cameraSourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      // set a key/value
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+
+      this.storage.set('0', this.selectedImage);
+
+      this.router.navigateByUrl(`displaytext`)
+    });
+  }
+
 
 }
