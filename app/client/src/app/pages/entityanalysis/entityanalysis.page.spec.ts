a6ab2a1713287cc584f3a2b8077935a82736e47b
Sonata Katt <sonata.abdullh@gmail.com>
2019-11-27 12:58:23 -0500

    add one more commented test

--- app/client/src/app/pages/entityanalysis/entityanalysis.page.spec.ts
+++ app/client/src/app/pages/entityanalysis/entityanalysis.page.spec.ts
@@ -21,7 +21,7 @@ describe('EntityanalysisPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
