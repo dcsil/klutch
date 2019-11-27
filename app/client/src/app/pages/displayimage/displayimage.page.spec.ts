d72a63ef72bf93b06ca0b6a4484a7fa46a829e6a
Sonata Katt <sonata.abdullh@gmail.com>
2019-11-27 13:44:35 -0500

    add passing test

--- app/client/src/app/pages/displayimage/displayimage.page.spec.ts
+++ app/client/src/app/pages/displayimage/displayimage.page.spec.ts
@@ -2,26 +2,65 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DisplayimagePage } from './displayimage.page';
+import { testUserAgent } from '@ionic/core/dist/types/utils/platform';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Storage } from '@ionic/storage';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject } from 'rxjs';
 
 describe('DisplayimagePage', () => {
   let component: DisplayimagePage;
   let fixture: ComponentFixture<DisplayimagePage>;
 
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  const storageIonicMock: any = {
+    get: () => new Promise<any>((resolve, reject) => resolve('test')),
+  };
+
+  const FirestoreStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    })
+  };
+
   beforeEach(async(() => {
+    
     TestBed.configureTestingModule({
       declarations: [ DisplayimagePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [ HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: Storage,
+          useValue: storageIonicMock
+        },
+        { provide: AngularFireStorage, useValue: FirestoreStub },
+        { provide: AngularFirestore, useValue: FirestoreStub }
+      ]
     })
     .compileComponents();
   }));
 
+
   beforeEach(() => {
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
     fixture = TestBed.createComponent(DisplayimagePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
